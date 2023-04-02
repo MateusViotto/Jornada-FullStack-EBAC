29db6692ff59deb76464e0797fff81a87f7901b6
Mateus Augusto Viotto <mateusviotto@gmail.com>
2023-04-02 18:28:17 -0300

    Aula 04
    Criação do sidebar e preparação para o banco de dados.

--- jornadadev/src/App.js
+++ jornadadev/src/App.js
@@ -5,10 +5,25 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-    <Video />
-    <Video />
-    <Video />
-    <Video />
+    <Video 
+      likes={100}
+      messages={200}
+      shares={300}
+      name="MateusViotto"
+      description="Gato goleiro"
+      music="musica épica"
+      url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=d1a44acd-bef3-4b18-bafe-92fa0b26828a"
+    />
+    <Video 
+      likes={100}
+      messages={200}
+      shares={300}
+      name="MateusViotto2"
+      description="Gato goleiro2"
+      music="musica épica2"
+      url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=d1a44acd-bef3-4b18-bafe-92fa0b26828a"
+    />
+
       </div>
     </div>
   );
--- jornadadev/src/pages/Video.js
+++ jornadadev/src/pages/Video.js
@@ -3,9 +3,10 @@ import React, {
   useState,
 } from "react"; /*É possível utilizar a função rcfe para criar o componente automaticamente*/
 import "./video.css";
+import VideoSideBar from "./components/sidebar/VideoSideBar";
 import VideoFooter from "./components/footer/VideoFooter";
 
-function Video() {
+function Video({ likes, messages, shares, name, description, music, url }) {
   const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
 
@@ -27,10 +28,14 @@ function Video() {
         onClick={handdleStart}
         loop /*Deixa o vídeo em loop*/
         /*controls    Adiciona o player do vídeo*/
-        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=d1a44acd-bef3-4b18-bafe-92fa0b26828a" /*Adicionar endereço do vídeo*/
+        src={url} /*Adicionar endereço do vídeo*/
       ></video>
-      {/*Side bar */}
-      <VideoFooter />
+      <VideoSideBar likes={likes} messages={messages} shares={shares}/>
+      <VideoFooter 
+      name={name}
+      description={description}
+      music={music}
+      />
     </div>
   );
 }
